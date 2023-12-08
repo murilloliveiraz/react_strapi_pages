329e13711dc7583fe58abd073603d00800bcd3d4
Murillo Oliveira <murillo1047@gmail.com>
2023-12-08 18:47:40 -0300

    feat: props of heading component

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800;900&family=Open+Sans:ital,wght@0,400;0,700;1,400&family=Poppins:wght@400;500;600;700;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,800;0,900;1,800&family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap');
 
   * {
     margin: 0;
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: ${({ theme }) => theme.font.family.default};
   }
 
-  h1{
+  h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.secondary};
   }
 `;
